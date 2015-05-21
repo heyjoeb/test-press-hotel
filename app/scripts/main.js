b08152ee0ba9f875440f1cc1a0c1d530e482721c
heyjoeb <heyjoeb@hotmail.com>
2015-05-21 17:35:06 -0500

    strucutre - header and slider ready

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+  /* jshint strict: false */
+  /*jshint camelcase: false */
+
+  //MAIN SLIDER
+  $('.bxslider').bxSlider({
+    auto: true,
+    randomStart: true,
+    pager: false,
+    speed: 800
+  });
+});
